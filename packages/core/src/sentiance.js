fa80ed9e251fac304191f96e025ccf60750bd868
Mohammed Aouf Zouag <mohammed.zouag@sentiance.com>
2022-03-22 10:35:14 +0100

    add script

--- packages/core/src/sentiance.js
+++ packages/core/src/sentiance.js
@@ -7,9 +7,13 @@ const core2 = function () {
 const core3 = function () {
     console.log("core3");
 }
+const core4 = function () {
+    console.log("core4");
+}
 
 module.exports = {
     core1,
     core2,
-    core3
+    core3,
+    core4
 };
