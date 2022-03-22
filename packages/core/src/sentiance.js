c6c496f978f5a22e5d0432fdb7b1370b910e812b
Mohammed Aouf Zouag <mohammed.zouag@sentiance.com>
2022-03-22 10:45:54 +0100

    commit

--- packages/core/src/sentiance.js
+++ packages/core/src/sentiance.js
@@ -7,13 +7,9 @@ const core2 = function () {
 const core3 = function () {
     console.log("core3");
 }
-const core4 = function () {
-    console.log("core4");
-}
 
 module.exports = {
     core1,
     core2,
-    core3,
-    core4
+    core3
 };
